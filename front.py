9ff2cef2db83e8b0cd940cb4a66838bcbd1625c7
AliaaMahmoud <34009995+AliaaMahmoud@users.noreply.github.com>
2019-05-03 16:24:04 +0200

    Update front.py

--- front.py
+++ front.py
@@ -2,31 +2,22 @@ from pytesseract import image_to_string
 import cv2
 import numpy as np
 im_gray = cv2.imread('front10.jpg', cv2.IMREAD_GRAYSCALE)
-#(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 im_bw = im_gray
-
-
 thresh = 90
 im_bw = cv2.threshold(im_bw, thresh, 255, cv2.THRESH_BINARY)[1]
 
 
-
-kernel2 = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-im_bw= cv2.filter2D(im_bw, -1, kernel2)
-im_bw= cv2.filter2D(im_bw, -1, kernel2)
-im_bw= cv2.filter2D(im_bw, -1, kernel2)
-
-cv2.imwrite('bla.png',im_bw)
 pic = im_gray[50:350,50:275]
 Name = im_bw[150:310, 400:1000]
-
 address = im_bw[300:450, 400:1000]
-
 ID = im_bw[500:560,400:1000]
-name=image_to_string(Name,lang="ara")
 
+
+name=image_to_string(Name,lang="ara")
 add=image_to_string(address,lang="ara")
 IDNumber=image_to_string(ID,lang="eng")
+
+
 IDNumber= ''.join(IDNumber.split())
 
 
@@ -37,6 +28,8 @@ else:
 month = IDNumber[3:5]
 day = IDNumber[5:7]
 BDate = year + '/' + month + '/'+ day
+
+
 ocr_output=open("text.txt","w+")
 ocr_output.write(name)
 ocr_output.write('\n')
