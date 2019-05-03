65ae8553d45d6e8181c70e3bc12a73d1d93851b3
AliaaMahmoud <34009995+AliaaMahmoud@users.noreply.github.com>
2019-05-03 16:09:00 +0200

    Update front.py

--- front.py
+++ front.py
@@ -1,7 +1,7 @@
 from pytesseract import image_to_string
 import cv2
 import numpy as np
-im_gray = cv2.imread('front3.jpg', cv2.IMREAD_GRAYSCALE)
+im_gray = cv2.imread('front10.jpg', cv2.IMREAD_GRAYSCALE)
 #(thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 im_bw = im_gray
 
@@ -11,29 +11,25 @@ im_bw = cv2.threshold(im_bw, thresh, 255, cv2.THRESH_BINARY)[1]
 
 
 
-kernel=np.ones((2,2),np.uint8)
-kernel1=np.ones((3,3),np.uint8)
-im_bw=cv2.dilate(im_bw,kernel,iterations = 1)
-im_bw=cv2.erode(im_bw,kernel1,iterations = 1)
-for i in range(8):
-	im_bw=cv2.blur(im_bw,(2,2))
-
 kernel2 = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+im_bw= cv2.filter2D(im_bw, -1, kernel2)
+im_bw= cv2.filter2D(im_bw, -1, kernel2)
 im_bw= cv2.filter2D(im_bw, -1, kernel2)
 
 cv2.imwrite('bla.png',im_bw)
 pic = im_gray[50:350,50:275]
-cv2.imwrite('ola.png',pic)
-Name = im_bw[150:315, 400:1000]
-cv2.imwrite('yarab2.png',Name)
+Name = im_bw[150:310, 400:1000]
+
 address = im_bw[300:450, 400:1000]
-cv2.imwrite('yarab.png',address)
+
 ID = im_bw[500:560,400:1000]
 name=image_to_string(Name,lang="ara")
+
 add=image_to_string(address,lang="ara")
 IDNumber=image_to_string(ID,lang="eng")
 IDNumber= ''.join(IDNumber.split())
-print(IDNumber)
+
+
 if IDNumber[0]=='2': 
 	year = '19' + IDNumber[1:3]
 else:
@@ -50,4 +46,3 @@ ocr_output.write(IDNumber)
 ocr_output.write('\n')
 ocr_output.write(BDate)
 ocr_output.close()
-
