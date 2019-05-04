92c842a072db4e5533509726793835d723a48c06
Mostafa Atef <m.atef96@hotmail.com>
2019-05-04 17:52:00 +0200

    Add files via upload

--- Scanner.py
+++ Scanner.py
@@ -6,46 +6,46 @@ import argparse
 import cv2
 import imutils
 
-# load the image and compute the ratio of the old height
-# to the new height, clone it, and resize it
-image = cv2.imread("Image.jpeg")
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height = 500)
+def scan(path):
+        # load the image and compute the ratio of the old height
+        # to the new height, clone it, and resize it
+        image = cv2.imread(path)
+        ratio = image.shape[0] / 500.0
+        orig = image.copy()
+        image = imutils.resize(image, height = 500)
 
-# convert the image to grayscale, blur it, and find edges
-# in the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-#edged = cv2.Canny(gray, 75, 200)
-edged = cv2.Canny(gray, 150, 200)
-
-
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
-
-# loop over the contours
-for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-		break
+        # convert the image to grayscale, blur it, and find edges
+        # in the image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        #edged = cv2.Canny(gray, 75, 200)
+        edged = cv2.Canny(gray, 150, 200)
 
 
-# apply the four point transform to obtain a top-down
-# view of the original image
-warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+        # find the contours in the edged image, keeping only the
+        # largest ones, and initialize the screen contour
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
 
-# show the original and scanned images
-cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-newimg = cv2.resize(warped,(1000,630))
-cv2.imwrite("ScannedImage.jpg",newimg)
-cv2.waitKey(0)
+        # loop over the contours
+        for c in cnts:
+                # approximate the contour
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+                # if our approximated contour has four points, then we
+                # can assume that we have found our screen
+                if len(approx) == 4:
+                        screenCnt = approx
+                        break
+
+
+        # apply the four point transform to obtain a top-down
+        # view of the original image
+        warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+        # show the original and scanned images
+        cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+        newimg = cv2.resize(warped,(1000,630))
+        return newimg
